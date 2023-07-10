194e24c327abb2ea4fc72aef24d16c65ce2ba451
Samith-hegde <hegdesamith02@gmail.com>
2023-07-10 20:20:00 +0530

    Bar chart added to frontend dashboard

--- crm/src/main.jsx
+++ crm/src/main.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import CustomerButtons from './CustomerForm'
-import Contacts from './Contacts'
+// import CustomerButtons from './CustomerForm'
+// import Contacts from './Contacts'
 import './main.css'
+import BarChart from './BarChart'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='main-container'>
+    {/* <div className='main-container'>
     <Contacts />
     <CustomerButtons />
-    </div>
+    </div> */}
+    <BarChart />
   </React.StrictMode>,
 )
